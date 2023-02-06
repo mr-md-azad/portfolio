09bcaf7cbf1ae43fa7c89d44c145d24091f1acea
Md Azad <81636077+mr-md-azad@users.noreply.github.com>
2023-02-06 20:51:39 +0530

    responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,8 +15,8 @@ const today = new Date();
         <meta name="description" content="Designed and Developed with ❤️ by @mr_md_azad" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="theme-color" content="#000000"></meta>
-        <link href='https://fonts.googleapis.com/css?family=Montserrat&display=optional'   />
-        <link href='https://fonts.googleapis.com/css?family=Poppins&display=optional'  />
+        <link href='https://fonts.googleapis.com/css?family=Montserrat&display=optional' />
+        <link href='https://fonts.googleapis.com/css?family=Poppins&display=optional' />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={style.main}>
@@ -25,38 +25,37 @@ const today = new Date();
           <div className="heading">I&apos;m Azad <br />ui designer &<br />web developer.</div>
 
           <div className="sub_heading"></div>
-          <a href='#about'><button className="about_btn "> about me</button></a>
+          <a href='#about'><button className="about_btn ">about me</button></a>
         </div>
 
         {/* 2. about section  */}
         <div className='about_section' id="about">
-        
           {/* about my self */}
           <div className="about_me about_box1">
-            <div className="heading">About me</div>
-            <div className="description">Let&apos;s know something about me,<br />My name is Md Azad (محمر آذار), {today.getFullYear()-2002} years old. An UI/UX Designer and Front-End Web Developer. Currently working as a Freelancer.</div>
-            <button className="skills_btn"> <a href='#skills' >My skills</a></button>
+            <div className="heading">ABOUT ME</div>
+            <div className="description">Let&apos;s know something about me,<br />My name is Md Azad (محمر آذار), {today.getFullYear() - 2002} years old. An UI/UX Designer and Front-End Web Developer. Currently working as a Freelancer.</div>
+            <button className="skills_btn"><a href='#skills'>My skills</a></button>
           </div>
 
           {/* my educations */}
           <div className="about_me about_box1">
-            <div className="heading">Educations</div>
+            <div className="heading">EDUCATIONS</div>
             <div className="description">
               <p>
                 <b>77.6% | CGPA-8.48</b><br />
-                Diploma in Computer Science & Engineering <br/>
-                Government Polytechnic, Chhapra <br/>
-                Board- SBTE Patna, Bihar 
-              </p> <br/>
+                Diploma in Computer Science & Engineering <br />
+                Government Polytechnic, Chhapra <br />
+                Board- SBTE Patna, Bihar
+              </p> <br />
               <p>
                 <b>76.4%</b><br />
-                Intermediate (XIIth), PCM<br/>
-                Board- BSEB Patna, Bihar 
-              </p><br/>
+                Intermediate (XIIth), PCM<br />
+                Board- BSEB Patna, Bihar
+              </p><br />
               <p>
                 <b>70.2%</b><br />
-                Matric (Xth)<br/>
-                Board- BSEB Patna, Bihar 
+                Matric (Xth)<br />
+                Board- BSEB Patna, Bihar
               </p>
             </div>
           </div>
@@ -72,20 +71,20 @@ const today = new Date();
           <div className="icons">
 
             <p>Languages</p>
-            <img src='/images/skills/html-5.svg' width={0}  height={0} alt='image'/>
+            <img src='/images/skills/html-5.svg' width={0} height={0} alt='image' />
             <img src='/images/skills/css3.svg' width={0} height={0} alt='image' />
-            <img src='/images/skills/javascript.svg' width={0}   height={0}alt='image'/>
-            <img src='/images/skills/c++.svg' width={0}   height={0}alt='image'/>
+            <img src='/images/skills/javascript.svg' width={0} height={0} alt='image' />
+            <img src='/images/skills/c++.svg' width={0} height={0} alt='image' />
 
 
             <p>Library/Framework</p>
-            <img src='/images/skills/tailwind-css.svg' width={0}  a height={0}lt='image'/>
-            <img src='/images/skills/react.svg' width={0}   height={0}alt='image'/>
+            <img src='/images/skills/tailwind-css.svg' width={0} a height={0} lt='image' />
+            <img src='/images/skills/react.svg' width={0} height={0} alt='image' />
 
             <p>Tools</p>
-            <img src='/images/skills/adobe-xd.svg' width={0}  height={0}alt='image' />
-            <img src='/images/skills/figma.svg' width={0}   height={0}alt='image'/>
-            <img src='/images/skills/visual-studio-code-2019.svg' width={0}  height={0}alt='image' />
+            <img src='/images/skills/adobe-xd.svg' width={0} height={0} alt='image' />
+            <img src='/images/skills/figma.svg' width={0} height={0} alt='image' />
+            <img src='/images/skills/visual-studio-code-2019.svg' width={0} height={0} alt='image' />
             <img className='github_icon' src='/images/skills/github.svg' width={0} height={0} alt='image' />
           </div>
 
