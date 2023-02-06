e61f8f2c8616797aa18a7caae5419d1a93f9e645
Md Azad <81636077+mr-md-azad@users.noreply.github.com>
2023-02-06 21:01:56 +0530

    error solved for responsivenes

--- src/pages/Navigation/Navigation.jsx
+++ src/pages/Navigation/Navigation.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import Link from 'next/link'
 import style from './Nav.module.scss'
@@ -17,24 +16,4 @@ const Navigation = () => {
   )
 }
 
-=======
-
-import Link from 'next/link'
-import style from './Nav.module.scss'
-
-const Navigation = () => {
-  return (
-    <>
-       <div className={style.navbar}>
-       <Link href='/'><img className={style.tittle} src='/images/Azad Sign LOGO.png' alt='image' width={0} height={0}/></Link>
-          <div className={style.links}>  
-            <Link href='https://www.linkedin.com/in/mdazad2' target='blank'><img src='/images/linkedin.png' width={30}  height={0}alt='image'/></Link>
-            <Link href='https://www.instagram.com/mr_md_azad/' target='blank'><img src='/images/instagram.png' width={40} height={0} alt='image'/></Link>
-          </div>
-       </div>
-    </>
-  )
-}
-
->>>>>>> 449c1b6a3a9dd21d001e21a5b0c90f23e6255f35
 export default Navigation
